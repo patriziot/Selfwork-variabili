72f91297487511f725f7624bae375a399f24fea7
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-25 17:56:49 +0200

    Selfwork variabili
    NOTE
    Non ho inteso bene se stampare a schermo si intendesse console o altro, così ho creato anche degli alert. La costante non consente sostituzioni e ovviamente dà errore.

---
+++ main.js
@@ -0,0 +1,22 @@
+// Variabile 1
+let num1 = 7;
+alert("Il valore della variabile num1 è"+num1);
+console.log("Il valore della variabile num1 è "+num1);
+
+// Variabile 2
+let num2 = 13;
+alert("Il valore della variabile num2 è"+num2);
+console.log("Il valore della variabile num2 è "+num2);
+
+// Stringa 1
+let stringa = "Hello Aulab";
+alert("Il valore della variabile stringa è "+stringa);
+console.log("Il valore della variabile stringa è "+stringa);
+stringa = true;
+console.log("la stringa è stata sostituita con "+stringa);
+
+// Costante 
+const PIGRECO = 3.14159265359;
+alert("Il valore fi PIGRECO è "+PIGRECO);
+console.log("Il valore fi PIGRECO è "+PIGRECO);
+PIGRECO = 3.456123;
